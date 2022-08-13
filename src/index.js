55aced4057ca2fb40db070d17adf01bc31f5acda
TheWisePigeon <zozozozeph@gmail.com>
2022-08-13 16:22:28 +0000

    added password material

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 const fs = require('fs')
-const fileName = '../test.json';
-const file = require(fileName);
-file.keyss = {
-  id:'3',
-  value:'jojo'
-};
+// const fileName = '../test.json';
+// const file = require(fileName);
+// file.ok = {
+//   id:'3',
+//   value:'jojo'
+// };
     
-fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
-  if (err) return console.log(err);
-  console.log(JSON.stringify(file));
-  console.log('writing to ' + fileName);
-});
+// fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
+//   if (err) return console.log(err);
+//   console.log(JSON.stringify(file));
+//   console.log('writing to ' + fileName);
+// });
+
+// var jsonObj = [{'Id':'1','Username':'Ray','FatherName':'Thompson'},  
+//                {'Id':'2','Username':'Steve','FatherName':'Johnson'},
+//                {'Id':'3','Username':'Albert','FatherName':'Einstein'}];
+
+// for (var i=0; i<jsonObj.length; i++) {
+//   if (jsonObj[i].Id == 3) {
+//     jsonObj[i].Username = "Thomas";
+//     break;
+//   }
+// }
+let test = fs.readFile('../test.json', 'utf-8', (err, data)=>{
+  console.log(data.toString());
+})
